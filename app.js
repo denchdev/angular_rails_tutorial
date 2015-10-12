50fd08d74f90ecbe020e21deecc8973dc27e95c0
denromani4 <denromani4@gmail.com>
2015-10-12 09:34:48 +0300

    init

---
+++ app.js
@@ -0,0 +1,59 @@
+angular.module('flapperNews', ['ui.router'])
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state ('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        })
+        .state ('posts', {
+          url: 'posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostCtrl'
+        });
+
+      $urlRouterProvider.otherwise('home');
+    }])
+
+  .factory('posts', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
+
+  .controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts){
+      $scope.posts = posts.posts;
+      
+      $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '') { return; }
+        $scope.posts.push({
+          title: $scope.title, 
+          link: $scope.link,
+          upvotes: 0,
+          comments: [
+            {author: 'Joe', body: 'Cool post!', upvotes: 0},
+            {author: 'Den', body: 'It\'s beatiful', upvotes: 0}
+          ]
+        });
+        $scope.title = '';
+        $scope.link = '';
+      };
+
+      $scope.incrementUpvotes = function (post) {
+        post.upvotes += 1;
+      }
+    }])
+  .controller( 'PostCtrl', [
+    '$scope',
+    '$stateParam',
+    'posts',
+    function($scope, $stateParam, posts) {
+      $scope.post = posts.posts[$stateParam.id];
+    }])
